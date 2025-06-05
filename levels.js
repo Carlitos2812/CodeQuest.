a3774f4214dc32cb2bd25360822e3a25e7160398
Carlitos2812 <carlosmartinez062812@gmail.com>
2025-06-05 09:37:55 -0500

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,42 @@
+const levels = [
+  {
+    question: "¿Cómo te llamas ____",
+    answer: "?"
+  },
+  {
+    question: "¡Qué sorpresa ____",
+    answer: "!"
+  },
+  {
+    question: "Mi color favorito es el azul ____ el tuyo ____",
+    answer: ","
+  },
+  {
+    question: "Compré manzanas ____ naranjas ____ y uvas.",
+    answer: ","
+  },
+  {
+    question: "Carlos dijo ____ Hoy no voy a salir ____",
+    answer: '"'
+  },
+  {
+    question: "Juan dijo ____ Estoy cansado ____",
+    answer: '"'
+  },
+  {
+    question: "Llegué a las 5 ____ 30 de la tarde.",
+    answer: ":"
+  },
+  {
+    question: "Mi casa está en Bogotá ____ Colombia.",
+    answer: ","
+  },
+  {
+    question: "Necesito: cuadernos ____ lápices ____ y reglas.",
+    answer: ","
+  },
+  {
+    question: "____Hola! ¿Cómo estás?",
+    answer: "¡"
+  }
+];
